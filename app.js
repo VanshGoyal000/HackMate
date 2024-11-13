430995738dc66bf4928811dac3dc761fb564dc50
vanshgoyal000 <vanshgoyal9528@gmail.com>
2024-11-13 20:03:31 +0530

    ejs dd

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
 app.use('/hackathons', hackathonRoutes);
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index.ejs'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
